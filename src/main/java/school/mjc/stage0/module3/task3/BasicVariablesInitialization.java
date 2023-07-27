7cfb8ff26df41403dac86ee7cb43ddd1bac55648
ElenaGeorgievska <elena_science@yahoo.com>
2023-07-27 16:58:35 +0200

    stage0 module3 task3 completed

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.module3.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        System.out.println("First variable is: " + first);
+        System.out.println("Second variable is: " + second );
+        System.out.println("Third variable is: " + third );
+
+        //Create 3 int variables with names: first, second, third.
+        // Initialize them with values: 1, 10, 100.
+        // Make console outputs for all of them, each on new line.
     }
 }
--- src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
+++ src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.module3.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        //write code here
+        int a;
+        int b;
+        int c;
+        a = b = c = 10;
+        System.out.println(c);
+
+        //Declare 3 int variables within one statement and call them a, b, c (order must be followed).
+        // Initialize them in one statement with value "10". Print to console "c".
+        // Implement the program inside of given code snippet:
     }
 }
